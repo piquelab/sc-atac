b5c02f02a17e3e6ad8a8997db4fee215d5f2518f
JulongWei <weijulong128@sina.com>
2021-02-16 10:32:14 -0500

    Add --group-list in the demux

---
+++ demux.2021-01-23/run_demuxOne.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -q express
+#SBATCH --partition=erprp
+#SBATCH --mem=120G
+#SBATCH --time=6-01:00:00
+#SBATCH -N 1-1
+#SBATCH -n 1
+
+set -v
+set -e
+
+cd /nfs/rprdata/julong/sc-atac/demux.2021-01-23
+echo $PWD
+
+module load demuxlet
+
+demuxFolder=demuxOut
+vcfFile=./VCF/sample6.posG100.MAF005.vcf.gz
+      
+popscle.2021-01-18 dsc-pileup \
+  --sam ../count.SCAIP.2021-01-14/${sample}/outs/possorted_bam.bam \
+  --group-list ../count.SCAIP.2021-01-14/${sample}/outs/filtered_peak_bc_matrix/barcodes.tsv \
+  --vcf ${vcfFile} \
+  --out ${demuxFolder}/${sample}.d > ${demuxFolder}/${sample}.errout.txt;
+popscle.2021-01-18 demuxlet --plp ${demuxFolder}/${sample}.d \
+  --group-list ../count.SCAIP.2021-01-14/${sample}/outs/filtered_peak_bc_matrix/barcodes.tsv \
+  --vcf ${vcfFile} \
+  --out ${demuxFolder}/${sample}.out --field GT --alpha 0.0 --alpha 0.5 --doublet-prior 0.1
---
+++ demux.2021-01-23/run_submit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /nfs/rprdata/julong/sc-atac/demux.2021-01-23
+
+#ls ../count.SCAIP.2021-01-14 |grep '^SCAIP' |sort >libList.txt
+# mkdir ./demuxOut/
+cat libList.txt |head -1 | \
+while read sample;
+do
+echo ${sample}
+sbatch --export=sample=${sample} --output=slurm.${sample}.out run_demuxOne.sh
+sleep 1
+done
