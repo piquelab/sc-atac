b4dfc4ba75ee4620a706147b743690958d731b46
JulongWei <weijulong128@sina.com>
2021-02-17 08:42:20 -0500

    rename file

---
+++ count.SCAIP.2021-01-14/run_submit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash/
+
+# fastqfolder=../fastq/
+# find ${fastqfolder} -name 'SCAIP*-ATAC*fastq.gz' | sed 's/.*\///;s/_S.*//' | grep -v 'Undet' | sort | uniq > libList.txt
+
+cat libList.txt | \
+while read sample; 
+do 
+sbatch --export=sample=${sample} --output=slurm.${sample}.output run_cell_ranger_count.sh
+sleep 0.5;
+done
+ 
