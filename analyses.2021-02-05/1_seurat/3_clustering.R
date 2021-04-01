141a29ab239cfe2c44d5c48ca3cceb77f46e756d
JulongWei <weijulong128@sina.com>
2021-04-01 16:33:30 -0400

    update clustering

--- analyses.2021-02-05/1_seurat/3_clustering.R
+++ analyses.2021-02-05/1_seurat/3_clustering.R
@@ -4,12 +4,15 @@
 source("../LibraryPackage.R")
 rm(list=ls())
 
+outdir <- "./3_clustering.outs/"
+if ( !file.exists(outdir)) dir.create(outdir, showWarnings=F, recursive=T)
+
 ###########################
 ### Clustering analysis ###
 ###########################
 
 if(FALSE){
-atac <- read_rds("./outs/1_seurat.merge.rds")
+atac <- read_rds("./outs/2_seurat.merge.SNG.rds")
                 
 atac2 <- RunTFIDF(atac)
 atac2 <- FindTopFeatures(atac2, min.cutoff="q0")
@@ -17,7 +20,7 @@ atac2 <- RunSVD(atac2,n=100)
 
 fig1 <- DepthCor(atac2)+
         theme(plot.title=element_text(size=10),plot.subtitle=element_text(size=10))
-figfn <- "./outs/Figure4.depthcor.png"
+figfn <- "./figures/Figure3.1_depthcor.png"
 png(figfn, width=600, height=400, res=120)
 print(fig1)
 dev.off()
@@ -27,18 +30,18 @@ atac2 <- RunUMAP(atac2, reduction="lsi",dims=2:50)
 atac2 <- FindNeighbors(atac2, reduction="lsi", dims=2:50)
 atac2 <- FindClusters(atac2, resolution=0.15, verbose=FALSE, algorithm=3)
 
-write_rds(atac2, file="./outs/2_seurat.cluster.rds")
+write_rds(atac2, file="./outs/3_seurat.cluster.rds")
 }
 
 ###############
 ### summary ###
 ###############
 
-if(TRUE){
-atac2 <- read_rds("./outs/2_seurat.cluster.rds")
+if(FALSE){
+atac2 <- read_rds("./3_clustering.outs/1_seurat.cluster.rds")
 
 fig2 <- DimPlot(object=atac2, label=TRUE, raster=F)+NoLegend()+theme_bw()+theme(legend.position="none")
-figfn <- "./outs/Figure5.1_cluster.png"
+figfn <- "./3_clustering.outs/Figure2.1_cluster.png"
 png(figfn, width=500, height=400, res=120)
 print(fig2)
 dev.off()
@@ -49,7 +52,7 @@ fig3 <- DimPlot(object=atac2, group.by="Batch", raster=F)+NoLegend()+
         theme_bw()+
         theme(legend.position="none",
               plot.title=element_blank())
-figfn <- "./outs/Figure5.2_BATCH.png"
+figfn <- "./3_clustering.outs/Figure2.2_BATCH.png"
 png(figfn, width=700, height=350, res=120)
 print(fig3)
 dev.off()
@@ -68,7 +71,7 @@ fig3 <- DimPlot(object=atac2, group.by="treat.alpha", raster=F)+NoLegend()+
         theme_bw()+
         theme(legend.position="none",
               plot.title=element_blank())
-figfn <- "./outs/Figure5.3_treat.png"
+figfn <- "./3_clustering.outs/Figure2.3_treat.png"
 png(figfn, width=650, height=650, res=120)
 print(fig3)
 dev.off()
