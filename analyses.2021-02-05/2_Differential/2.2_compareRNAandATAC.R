14b420e3640b87bb7dec0bdbaedbcf53dc37516f
JulongWei <weijulong128@sina.com>
2021-10-12 22:52:28 -0400

    test if DEGs or DVGs are enriched in DARs

--- analyses.2021-02-05/2_Differential/2.2_compareRNAandATAC.R
+++ analyses.2021-02-05/2_Differential/2.2_compareRNAandATAC.R
@@ -244,7 +244,7 @@ dev.off()
 
 
 ###
-### 
+### flanking region
 ### generate data for qq plot
 comb <- sort(unique(resDE$comb))
 dfNew <- map_dfr(comb, function(ii){
@@ -287,4 +287,221 @@ png(figfn, width=750, height=750, res=120)
 print(p2)
 dev.off()
 
+
+
+
+############################
+### if enrich are in DVG ###
+############################
+
+### previous identified DEGs
+fn <- "/nfs/rprdata/julong/SCAIP/analyses/SCAIP-B1-6_2020.03.23/10_RNA.Variance_output/tmp9/3_phiNew.meta"
+resDE <- read.table(fn,header=T)%>%dplyr::filter(qval<0.1, abs(beta)>0.5)
+resDE <- resDE%>%
+   mutate(comb=paste(MCls, contrast, sep="_"))
+
+
+###
+### qq for cloest genes
+comb <- sort(unique(resDE$comb))
+dfNew <- map_dfr(comb, function(ii){
+  res2 <- res%>%dplyr::filter(comb==ii)
+  DEG <- resDE%>%dplyr::filter(comb==ii)%>%dplyr::pull(gene)
+  res2 <- res2%>%mutate(is_DEG=ifelse(geneId%in%DEG,1,0))  
+  ###
+  dx <- map_dfr(c(0,1),function(i){
+     di <- res2%>%dplyr::filter(is_DEG==i)%>%arrange(p.value)
+     ngene <- nrow(di)
+     di <- di%>%mutate(observed=-log10(p.value), expected=-log10(ppoints(ngene)))
+     di
+  })
+  dx
+})
+
+###
+###
+lab1 <- c("LPS"="LPS", "LPS-DEX"="LPS+DEX", "PHA"="PHA", "PHA-DEX"="PHA+DEX")
+p1 <- ggplot(dfNew, aes(x=expected,y=observed, colour=factor(is_DEG)))+
+   ggrastr::rasterise(geom_point(size=0.3),dpi=300)+
+   geom_abline(colour="red")+
+   scale_colour_manual(values=c("0"="grey40","1"="green"),
+      labels=c("0"="Not DVG", "1"="DVG"),
+      guide=guide_legend(override.aes=list(size=1)))+
+   facet_grid(MCls~contrast, scales="free", labeller=labeller(contrast=lab1))+
+   xlab(bquote("Expected"~ -log[10]~"("~italic(plain(P))~")"))+
+   ylab(bquote("Observed"~-log[10]~"("~italic(plain(P))~")"))+
+   theme_bw()+
+   theme(legend.title=element_blank(),strip.text=element_text(size=12))
+
+figfn <- "./2.2_compareRNAandATAC.outs/Figure2.5_qq.DVG.cloest.png"
+png(figfn, width=750, height=750, res=120)
+print(p1)
+dev.off()
+
+
+
+##################################
+### DEG if is enriched in DARs ###
+##################################
+
+### fisher test function
+cal.fisher <- function(df){
+   ###
+ resfisher <- map_dfr(1:nrow(df),function(i){
+      dmat <- matrix(as.numeric(df[i,]), 2, 2)
+      colnames(dmat) <- c("interest", "not.interest")
+      rownames(dmat) <- c("in.DAR", "not.DAR")
+      res <- fisher.test(dmat)
+      res2 <- data.frame(odds=res$estimate, pval.fisher=res$p.value,
+         lower=res$conf.int[1], upper=res$conf.int[2])
+      res2
+   })
+   resfisher
+}
+
+### 
+df.fisher <- function(res, resDP, peakAnno){
+   ### 
+   comb <- sort(unique(res$comb))
+   df.ls <- lapply(1:length(comb), function(i){
+      ii <- comb[i] 
+      cell <- gsub("_.*", "", ii)
+      contrast <- gsub(".*_", "", ii)
+      res2 <- res%>%dplyr::filter(comb==ii) ## dplyr::filter(qval<0.1, abs(beta)>0.5)
+      resDP2 <- resDP%>%
+         dplyr::filter(comb==ii, p.adjusted<0.1, abs(estimate)>0.5)%>%
+         dplyr::left_join(peakAnno, by="peak")
+      if( nrow(resDP2)>5){
+         sigGene <- res2%>%
+            dplyr::filter(qval<0.1, abs(beta)>0.5)%>%
+            dplyr::pull(gene)
+         ###
+         interest.in.DARs <- sum(sigGene%in%resDP2$geneId)
+         interest.not.DARs <- length(sigGene)-interest.in.DARs
+      ###
+         notSig <- setdiff(res2$gene, sigGene)
+         not.interest.in.DARs <- sum(notSig%in%resDP2$geneId)
+         not.interest.not.DARs <- length(notSig)-not.interest.in.DARs
+         df2 <- data.frame("interest.in.DARs"=interest.in.DARs,
+            "interest.not.DARs"=interest.not.DARs,
+            "not.interest.in.DARs"=not.interest.in.DARs,
+            "not.interest.not.DARs"=not.interest.not.DARs)
+         df2$cell <- cell
+         df2$contrast <- contrast
+         df2$comb <- ii
+      }else{
+        df2 <- NA    
+      }
+      df2
+   })    
+   df.ls <- df.ls[!is.na(df.ls)]
+   df <- do.call(rbind, df.ls)    
+   res <- cal.fisher(df[,1:4])
+   res2 <- cbind(df, res)
+   ### 
+   as.data.frame(res2)
+}    
+   
+    
+
+#################
+### read data ###
+#################
+
+
+
+fn <- "./1.2_DiffPeak.outs/2.0_DESeq.results.rds"
+resDP <- read_rds(fn)%>%drop_na(p.value)%>%
+   mutate(comb=paste(MCls, contrast, sep="_"))%>%
+   dplyr::rename("peak"="gene") 
+
+
+fn <- "./2.2_compareRNAandATAC.outs/2_annot.ChIPseeker.rds"
+peakAnno <- read_rds(fn)%>%as.data.frame()%>%
+   mutate(peak=paste(gsub("chr", "", seqnames), start, end, sep="-")) 
+peakAnno2 <- peakAnno%>%dplyr::select(peak, geneId, SYMBOL)
+
+
+
+### test if DEG is DARs 
+fn <- "/nfs/rprdata/julong/SCAIP/analyses/SCAIP-B1-6_2020.03.23/6_DEG.CelltypeNew_output/Filter2/2_meta.rds"
+res <- read_rds(fn)%>%drop_na(pval)%>%
+   mutate(comb=paste(MCls, contrast, sep="_"))%>%
+   as.data.frame()
+##
+resFisher <- df.fisher(res, resDP, peakAnno2)
+opfn <- "./2.2_compareRNAandATAC.outs/3.1_enrich.DEG.csv"
+write.csv(resFisher, opfn, row.names=F)
+
+
+### test if DVGs is DARs
+fn <- "/nfs/rprdata/julong/SCAIP/analyses/SCAIP-B1-6_2020.03.23/10_RNA.Variance_output/tmp9/3_phiNew.meta"
+res <- read.table(fn, header=T) %>%drop_na(pval)%>%
+   mutate(comb=paste(MCls, contrast, sep="_"))
+
+resFisher2 <- df.fisher(res, resDP, peakAnno2)
+opfn <- "./2.2_compareRNAandATAC.outs/3.2_enrich.DVG.csv"
+write.csv(resFisher2, opfn, row.names=F)
+
+
+
+
+#######################
+### error bar plots ###
+#######################
+
+col.treat <- c("LPS"="#fb9a99", "LPS-DEX"="#e31a1c",
+             "PHA"="#a6cee3", "PHA-DEX"="#1f78b4")
+col.MCl <- c("Bcell"="#4daf4a", "Monocyte"="#984ea3",
+             "NKcell"="#aa4b56", "Tcell"="#ffaa00")
+
+fn <- "./2.2_compareRNAandATAC.outs/3.1_enrich.DEG.csv"
+df <- read.csv(fn, header=T)
+df2 <- data.frame(odds=df$odds,
+   CI.low=df$lower, CI.high=df$upper,
+   comb=gsub("_", ".", df$comb),
+   MCls=df$cell, contrast=df$contrast)
+##                  
+p1 <- ggplot(df2, aes(x=odds, y=comb))+
+   geom_errorbarh(aes(xmax=CI.high, xmin=CI.low, colour=MCls),
+       size=0.5, height=0.2)+
+   geom_point(aes(colour=MCls), shape=19, size=1.5)+
+   scale_colour_manual(values=col.MCl)+
+   geom_vline(aes(xintercept=1), size=0.25, linetype="dashed")+ 
+   xlab("Odds ratio")+
+   ggtitle("DEGs are enriched in DARs")+    
+   theme_bw()+
+   theme(plot.title=element_text(hjust=0.5, size=9),
+         axis.title.y=element_blank(),
+         axis.title.x=element_text(size=9),
+         axis.text=element_text(size=8),
+         legend.position="none")
+
+###
+fn <- "./2.2_compareRNAandATAC.outs/3.2_enrich.DVG.csv"
+df <- read.csv(fn, header=T)
+df2 <- data.frame(odds=df$odds,
+   CI.low=df$lower, CI.high=df$upper,
+   comb=gsub("_", ".", df$comb),
+   MCls=df$cell, contrast=df$contrast)
+p2 <- ggplot(df2, aes(x=odds, y=comb))+
+   geom_errorbarh(aes(xmax=CI.high, xmin=CI.low, colour=MCls),
+       size=0.5, height=0.2)+
+   geom_point(aes(colour=MCls), shape=19, size=1.5)+
+   scale_colour_manual(values=col.MCl)+
+   geom_vline(aes(xintercept=1), size=0.25, linetype="dashed")+ 
+   xlab("Odds ratio")+
+   ggtitle("DVGs are enriched in DARs")+    
+   theme_bw()+
+   theme(plot.title=element_text(hjust=0.5, size=9),
+         axis.title.y=element_blank(),
+         axis.title.x=element_text(size=9),
+         axis.text=element_text(size=8),
+         legend.position="none")
+
+###
+figfn <- "./2.2_compareRNAandATAC.outs/Figure3_enrich.DAR.png"
+png(figfn, width=680, height=480, res=120)
+plot_grid(p1, p2, ncol=2)
+dev.off()
 
