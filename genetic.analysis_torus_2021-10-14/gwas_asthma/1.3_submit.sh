7ef4ee2607cc4d6afcb7b0cf4dff4bdbd8eedb3a
JulongWei <weijulong128@sina.com>
2022-12-08 09:14:00 -0500

    summary number of SNPs in each annotation

--- genetic.analysis_torus_2021-10-14/gwas_asthma/1.3_submit.sh
+++ genetic.analysis_torus_2021-10-14/gwas_asthma/1.3_submit.sh
@@ -5,15 +5,15 @@ cd $PWD
  
 # oneMCl="Tcell"
 
-for oneMCl in Bcell Monocyte NKcell Tcell; do
-
+# for oneMCl in Bcell Monocyte NKcell Tcell; do
+oneMCl="comb"
 cat motif_split.txt |
 while read motifFile; do
    sbatch -q express -p erprp --mem=8G --time=1-00:00:00 -N 1-1 -n 1 --output=slurm_${oneMCl}_${motifFile}.output --wrap "
    module load R;
    R CMD BATCH '--args ${motifFile} ${oneMCl}' 1.3_summary_annot.R torm_${oneMCl}_${motifFile}.Rout"
    echo ${oneMCl} ${motifFile}
-   sleep 1;
+##   sleep 1;
 done 
 
-done
+###done
--- genetic.analysis_torus_2021-10-14/gwas_asthma/1.3_summary_annot.R
+++ genetic.analysis_torus_2021-10-14/gwas_asthma/1.3_summary_annot.R
@@ -13,27 +13,31 @@ if (length(args)>0){
    oneMCl <- "Bcell"
 }
  
-outdir <- paste("./torus_input/Motif_", oneMCl, "/", sep="")
+outdir <- paste("./torus_input/summary_", oneMCl, "/", sep="")
 if ( !file.exists(outdir)) dir.create(outdir, showWarnings=F, recursive=T)
+
 
 ###
 fn <- paste("./Motif_file/", motifFile, sep="")
 motifList <- read.table(fn)$V1
+
+
 ###
 summ <- map_dfr(1:length(motifList), function(i){
    ## 
    motif <- motifList[i] 
    cat(i, motif, "\n")    
    ###
-   fn <- paste(outdir, motif, "_union_torus.annot.gz", sep="")
+   ## fn <- paste(outdir, motif, "_union_torus.annot.gz", sep="") ## for cell-types
+   fn <- paste("./torus_input/Motif_", oneMCl, "/",  motif, "_torus.annot.gz", sep="") 
    if ( file.exists(fn)){
       ### 
       annot <- fread(fn, header=T, data.table=F)
-      summ2 <- data.table(motif=motif, MCls=oneMCl, num=sum(annot$peaking_d==2)) 
+      summ2 <- data.table(motif=motif, MCls=oneMCl, num=sum(annot$motif_d==1)) 
       summ2
    }    
 })
 
-opfn <- paste("./torus_input/Summary_", oneMCl, "_", motifFile, ".txt", sep="")
+opfn <- paste(outdir, "summ_", motifFile, ".txt", sep="")
 write.table(summ, opfn, quote=F, row.names=F, col.names=F)
 ### End
