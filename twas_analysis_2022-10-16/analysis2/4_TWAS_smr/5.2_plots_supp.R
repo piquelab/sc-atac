fd8b7925d389bc3e5fda8d58d8ef2a946af02005
JulongWei <weijulong128@sina.com>
2024-08-26 16:39:16 -0400

    Forest plots of asthma-risk-genes in DEGs

--- twas_analysis_2022-10-16/analysis2/4_TWAS_smr/5.2_plots_supp.R
+++ twas_analysis_2022-10-16/analysis2/4_TWAS_smr/5.2_plots_supp.R
@@ -131,21 +131,192 @@ dev.off()
 
 
 
+########################
+### manhattan plots ####
+########################
+  
+
+###
+## fn_ls <- list.files ("./Asthma_twas/", pattern="ALOFT|Whole") ## |Whole
+## ii <- fn_ls[3]
+
+### 
+autosome <- as.character(1:22) 
+grch38_unq <- grch38%>%
+    dplyr::filter(chr%in%autosome, grepl("protein", biotype))%>%
+    distinct(ensgene, chr, .keep_all=T)%>%dplyr::select(Gene=ensgene, start)
+
+
+###
+### read twas data 
+fn <- "./5_pub.outs/2_supp_tables/TableS6_1_asthma-risk-genes_gtex.txt.gz"
+res <- read.table(fn, header=T, sep="\t")
+res <- res%>%left_join(grch38_unq, by="Gene")
+names(res)[4] <- "chr"
+
+res <- res%>%mutate(log10p=-log10(pval_twas), gr2=factor(chr%%2))
+
+### 
+### add x-axis information 
+ngene <- nrow(res)
+nchr <- max(res$chr)
+ 
+###
+res$BPcum <- NA
+s <- 0
+for (i in 1:nchr){
+   x <- res[res$chr==i, "start"]+s
+   res[res$chr==i,"BPcum"] <- x
+   s <- max(x)
+}
+
+axis.set <- res%>%
+   group_by(chr)%>%
+   summarize(midpoint=(max(BPcum)+min(BPcum))/2)%>%ungroup()
+
+
+
+sig <- res%>%filter(FDR_twas<0.1)%>%pull(log10p)%>%min()
+
+
+###
+### annotation gene name
+x <- res%>%dplyr::filter(FDR_twas<0.05)%>%arrange(pval_twas)
+x1 <- x[1:20,]
+x2 <- x%>%dplyr::filter(FDR_intact<0.05, peaking_d==3)
+x2 <- x2[1:50,]
+annoDF2 <- rbind(x1,x2)%>%distinct(symbol, .keep_all=T)
+
+
+ymax <- max(res$log10p)+8
+p1 <- ggplot(res)+
+   geom_point(aes(x=BPcum, y=log10p, color=factor(gr2), size=log10p))+
+   geom_text_repel(data=annoDF2,
+       aes(x=BPcum, y=log10p, label=symbol), box.padding=0.2,
+           min.segment.length=0, segment.curvature=1e-20, 
+            max.overlaps=15, fontface="italic", size=3)+      
+   geom_hline(yintercept=sig, color="red", linetype="dashed")+ 
+   scale_x_continuous("chromosome", label=axis.set$chr, breaks=axis.set$midpoint,
+      limits=c(min(res$BPcum), max(res$BPcum)) )+
+   scale_y_continuous(bquote(-log[10]~"("~italic(p)~")"), limits=c(0,ymax))+
+   scale_color_manual(values=c("0"="#2171b5", "1"="#6baed6"))+
+   scale_size_continuous(range=c(0.1,1.5)) +
+   theme_bw()+
+   theme(legend.position="none",
+         axis.title=element_text(size=12),
+         axis.text.x=element_text(size=10, angle=90, vjust=0.5, hjust=1),
+         axis.text.y=element_text(size=12),
+         panel.grid.major=element_blank(),
+         panel.grid.minor=element_blank())
+
+
+###
+### QQ plots 
+ci <- 0.95 
+plotDF <- res%>%dplyr::select(Gene, pval=pval_twas)%>%arrange(pval)%>%
+    mutate(observed=-log10(pval), expected=-log10(ppoints(ngene)),
+           clower=-log10(qbeta(p=(1-ci)/2, shape1=seq(ngene), shape2=rev(seq(ngene)))),
+           cupper=-log10(qbeta(p=(1+ci)/2, shape1=seq(ngene), shape2=rev(seq(ngene)))) )
+           
+p2  <- ggplot(plotDF, aes(x=expected, y=observed))+
+   geom_point(color="#d01c8b", size=1)+
+   ## geom_ribbon(aes(ymax=cupper, ymin=clower), fill="grey", alpha=0.5)+ 
+   geom_abline(slope=1, intercept=0)+
+   xlab(bquote("Expected"~ -log[10]~"("~italic(plain(P))~")"))+
+   ylab(bquote("Observed"~-log[10]~"("~italic(plain(P))~")"))+
+   theme_bw()+
+   theme(legend.position="none",
+         axis.text=element_text(size=12),
+         axis.title=element_text(size=12),
+         panel.grid.major=element_blank(),
+         panel.grid.minor=element_blank())
+
+## p2 <- as.grob(p2)
+
+
+plot_comb <- plot_grid(p1, p2, rel_widths=c(1.7, 1),
+    labels=c("A", "B"), label_fontface="plain", nrow=1, ncol=2, aligh="h", axis="tb")
+
+
+###
+figfn <- paste(outdir, "FigS5_2_asthma_manhattan_qq_comb.GTEx.png", sep="")
+png(figfn, width=1200, height=450, pointsize=12, res=120)
+print(plot_comb)
+dev.off()
+
+###
+figfn <- paste(outdir, "FigS5_2_asthma_manhattan_qq_comb.GTEx.pdf", sep="")
+pdf(figfn, width=12, height=4.5)
+print(plot_comb)
+dev.off()
 
 
 
 
 
+###########################################################
+### FigS5_3 Forest plots if enrichment in DEGs 
+###########################################################
+
+
+
+
+fn <- "./5_pub.outs/2_supp_tables/Figure_S6_1_DEG_enrich.xlsx"
+plotDF <- read.xlsx(fn)
+
+
+col1 <- c("Bcell"="#4daf4a", "Monocyte"="#984ea3", "NKcell"="#aa4b56", "Tcell"="#ffaa00")
+col2 <- c("LPS"="#fb9a99", "LPS+DEX"="#e31a1c", "PHA"="#a6cee3", "PHA+DEX"="#1f78b4")
+
+
+plotDF <- plotDF%>%
+    mutate(condition2=gsub("-", "+", conditions), MCls=gsub("_.*", "", conditions),
+           contrast=gsub("-", "+", gsub(".*_", "", conditions)),
+           log_odds=log(odds), log_lower=log(CI_lower), log_upper=log(CI_upper))
+
+p <- ggplot(plotDF, aes(x=log_odds, y=condition2))+
+   geom_errorbarh(aes(xmax=log_upper, xmin=log_lower, colour=contrast),
+       size=0.5, height=0.2)+ 
+   geom_point(aes(colour=contrast), shape=19, size=1.5)+
+   scale_colour_manual(values=col2)+
+   geom_vline(aes(xintercept=0), size=0.25, linetype="dashed")+ 
+   xlab("log odds ratio")+xlim(-3, 1)+
+   ###scale_y_discrete(labels=ylab2)+ 
+   theme_bw()+
+   theme(##plot.title=element_text(hjust=0.5, size=14),
+         axis.title.y=element_blank(),
+         axis.title.x=element_text(size=10),
+         axis.text.x=element_text(size=10),
+         axis.text.y=element_text(size=10),
+         ## legend.position="none")
+         legend.title=element_blank(),
+         legend.text=element_text(size=9),
+         legend.key.size=unit(0.4, "cm"))
+         ## legend.position="none")
+
+
+figfn <- paste(outdir, "FigS5_3_olapDEG_enrich.forest.pdf", sep="")
+pdf(figfn, width=5.5, height=5)
+print(p)
+dev.off()
+
+## figfn <- paste(outdir, "Figure_S6_1_DEG_enrich.forest.png", sep="")
+## ggsave(figfn, p, device="png", width=550, height=480, units="px", dpi=120)    
 
 
 
 
 
+#############
+##############
 
 
 
 
 
+############################
+### enrichment analysis ####
+#############################
 
 
 
